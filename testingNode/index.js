3bc35beff9462f5f2c9e8f5dbd350332942e4ef7
ErlendS <erlend.seppola@gmail.com>
2016-07-26 14:34:13 +0200

    Testing

---
+++ testingNode/index.js
@@ -0,0 +1,11 @@
+console.log('Starting node script')
+
+var postfix = 'thing..'
+var prefix = 'Number: '
+
+for (var i = 0; i < 10; i++) {
+  console.log(`Ooooh ${i} ${postfix}`)
+}
+
+
+console.log('Node script end')
