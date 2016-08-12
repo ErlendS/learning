e25fa5b90029fde55e35601805dd0bcc91644bd9
ErlendS <erlend.seppola@gmail.com>
2016-08-12 20:16:53 +0200

    created a sort function that sorts any ordred array to lowest to highest. created test functions that compares each value in the sorted array to check if it is correct.

--- cards/__tests__/deck-spec.js
+++ cards/__tests__/deck-spec.js
@@ -1,14 +1,46 @@
 const test = require('ava')
-const deck = require('../deck.js')
+const Deck = require('../deck.js')
 
-test('deck.generateSuit – generates 13 cards of a suit', t => {
-  t.true(deck.generateSuit('S').length === 13)
+test('Deck.generateSuit – generates 13 cards of a suit', t => {
+  t.true(Deck.generateSuit('S').length === 13)
 })
 
-test('deck.generateSuit – generates returns only strings prefixed with the suit', t => {
-  const spades = deck.generateSuit('S')
+test('Deck.generateSuit – generates returns only strings prefixed with the suit', t => {
+  const spades = Deck.generateSuit('S')
   spades.forEach(card => t.true(card.charAt(0) === 'S'))
 
-  const hearts = deck.generateSuit('H')
+  const hearts = Deck.generateSuit('H')
   hearts.forEach(card => t.true(card.charAt(0) === 'H'))
+})
+
+test('Deck.shuffle – shuffles a deck', t => {
+  const originalDeck = Deck.generateDeck()
+  const newDeck = Deck.shuffle(originalDeck)
+  let i = 0
+
+  newDeck.forEach((card, index) => {
+    if (originalDeck[index] === newDeck[index]) {
+      i += 1;
+    }
+  })
+
+  t.true(i < 26, 'this may fail in VERY rare cases, run twice if it does')
+})
+
+test('Deck.createSortFn – composes a function to sort a deck of cards', t => {
+  const deck = Deck.shuffle(Deck.generateDeck())
+  const compareFn = Deck.compareCardValues
+  const sort = Deck.createSortFn(compareFn)
+
+  t.true(typeof sort === 'function')
+  const sortedDeck = sort(deck)
+  t.false(sortedDeck === deck, 'returns a new deck')
+
+  t.plan(deck.length + 2)
+  let lastValue = 0
+  sortedDeck.forEach(card => {
+    const value = Deck.cardValue(card)
+    t.true(value >= lastValue)
+    lastValue = value
+  })
 })
--- cards/deck.js
+++ cards/deck.js
@@ -1,4 +1,7 @@
-// Lag en kortstokk!
+const R = require('ramda')
+
+
+const SUIT_ORDER = ['S', 'C', 'H', 'D']
 
 // lag en funksjon som genererer array med 52 string values som en vanlig kortstokk.
 
@@ -10,16 +13,97 @@ function generateSuit(suitType) {
   return suitArray;
 }
 
-function generateCards() {
+function generateDeck() {
   let deck = [];
-  const suits = "SCHD"
+  const suits = SUIT_ORDER
   for (var i = 0; i < 4; i++) {
-      deck = deck.concat(generateSuit(suits.charAt(i)))
+      deck = deck.concat(generateSuit(suits[i]))
   }
   return deck;
 }
 
+ // omplasser to tilfeldige verdier i deck arrayen 15000 ganger
+ function shuffle(_deck) {
+   const deck = R.clone(_deck)
+
+   // velg et tilfeldig tall mellom 0-51
+   function positionInArray() {
+     return Number.parseInt(Math.random() * 1000000 % 52)
+   }
+   for(var i = 0; i < 15000; i++) {
+     let firstPosition = positionInArray()
+     let secondPosition = positionInArray()
+     let anyCard = deck[firstPosition]
+     deck[firstPosition] = deck[secondPosition]
+     deck[secondPosition] = anyCard
+   }
+   return deck;
+ }
+
+
+function insertIntoSortedArray(x, sortedArray, compareFn) {
+  // sammenligner x mot sortedArray og legger x inn i sortedArray
+  if (sortedArray.length === 0) {
+    sortedArray.push(x)
+    return sortedArray
+  }
+
+  let done = false
+  for (let i = 0; i < sortedArray.length; i++) {
+    if (compareFn(sortedArray[i], x) === 1 || compareFn(sortedArray[i], x) === 0) {
+      sortedArray.splice(i, 0, x)
+      done = true
+      break
+    }
+  }
+  if (!done) {
+    sortedArray.push(x)
+  }
+  return sortedArray
+}
+
+function createSortFn(compareFn) {
+  return function sort(deck) {
+    let currentCard = deck[0]
+    let newDeck = []
+    for (let i = 0; i < deck.length; i++) {
+      currentCard = deck[i]
+      newDeck = insertIntoSortedArray(currentCard, newDeck, compareFn)
+    }
+    return newDeck
+  }
+}
+
+function cardValue(card) {
+  const [suit, value] = card.split("_")
+  return Number.parseInt(value, 10)
+}
+
+function cardSuit(card) {
+    const [suit] = card.split("_")
+    return suit
+}
+
+function compareCardValues (card1, card2) {
+  const v1 = cardValue(card1)
+  const v2 = cardValue(card2)
+  if (v1 > v2 ) {
+    return 1
+  }
+  if (v1 == v2) {
+    return 0
+  }
+  if (v1 < v2) {
+    return -1
+  }
+}
+
+
  module.exports = {
-   generateCards,
+   generateDeck,
    generateSuit,
+   shuffle,
+   cardValue,
+   createSortFn,
+   compareCardValues,
  }
